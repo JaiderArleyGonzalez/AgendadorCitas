5819df47d5a8b71c4deb9b865404c0cd9bad6938
Jaider Gonzalez <jaidergonzalez1@gmail.com>
2023-05-08 20:55:04 -0500

    Se añaden parámetros adicionales #54

--- src/main/java/edu/eci/cvds/Evento.java
+++ src/main/java/edu/eci/cvds/Evento.java
@@ -1,37 +1,17 @@
 package edu.eci.cvds;
-
-import java.io.Serializable;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import javax.annotation.PostConstruct;
-import javax.faces.application.FacesMessage;
-import javax.faces.bean.ViewScoped;
-import javax.faces.context.FacesContext;
-import javax.faces.event.AjaxBehaviorEvent;
-import javax.faces.model.SelectItem;
-import javax.inject.Inject;
-import javax.inject.Named;
-
-import org.primefaces.event.SelectEvent;
-import org.primefaces.event.schedule.ScheduleEntryMoveEvent;
-import org.primefaces.event.schedule.ScheduleEntryResizeEvent;
-import org.primefaces.event.schedule.ScheduleRangeEvent;
 import org.primefaces.model.DefaultScheduleEvent;
-import org.primefaces.model.DefaultScheduleModel;
-import org.primefaces.model.LazyScheduleModel;
-import org.primefaces.model.ScheduleDisplayMode;
-import org.primefaces.model.ScheduleEvent;
-import org.primefaces.model.ScheduleModel;
+
 
 public class Evento extends DefaultScheduleEvent{
     private boolean casoAsiloTurista;
     private boolean negocioEUU;
+    private String nombre;
+    private String apellido;
+    private int numeroTelefono;
+    private String correoElectronico;
+    private String descripcionUsuario;
+    private String estadoCita;
+    
     public void setCasoAsiloTurista(boolean respuesta){
         casoAsiloTurista = respuesta;
     }
@@ -44,6 +24,44 @@ public class Evento extends DefaultScheduleEvent{
     public boolean getCasoAsiloTurista(){
         return casoAsiloTurista;
     }
+
+    public String getApellido() {
+        return apellido;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
     
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public int getNumeroTelefono() {
+        return numeroTelefono;
+    }
+    public void setNumeroTelefono(int numeroTelefono) {
+        this.numeroTelefono = numeroTelefono;
+    }
     
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+    
+    public String getDescripcionUsuario() {
+        return descripcionUsuario;
+    }
+    public void setDescripcionUsuario(String descripcionUsuario) {
+        this.descripcionUsuario = descripcionUsuario;
+    }
+    public String getEstadoCita() {
+        return estadoCita;
+    }
+    public void setEstadoCita(String estadoCita) {
+        this.estadoCita = estadoCita;
+    }
 }
