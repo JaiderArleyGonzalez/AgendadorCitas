be2c1fd79c677109fd3a6e47a2a1491b59f12be1
BRIAN CAMILO FAJARDO SANCHEZ <brian.fajardo@mail.escuelaing.edu.co>
2023-05-16 23:01:21 -0500

    Prueba repo base

--- src/main/java/edu/eci/cvds/Main.java
+++ src/main/java/edu/eci/cvds/Main.java
@@ -10,7 +10,7 @@ import javax.servlet.ServletContext;
 import java.util.Arrays;
 
 @SpringBootApplication
-public class Main extends SpringBootServletInitializer {
+public class Main {
     @Autowired
   public static void main (String[] args) {
       SpringApplication.run(Main.class, args);
