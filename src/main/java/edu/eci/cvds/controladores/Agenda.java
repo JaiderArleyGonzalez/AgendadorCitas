93b6cadac544fa7ce9fd0931ad325fc172cbc2e1
Miguel Angel Barrera Diaz <miguel.barrera-d@mail.escuelaing.edu.co>
2023-05-31 18:23:16 -0500

    Arreglos front

--- src/main/java/edu/eci/cvds/controladores/Agenda.java
+++ src/main/java/edu/eci/cvds/controladores/Agenda.java
@@ -1,5 +1,10 @@
 package edu.eci.cvds.controladores;
 
+import java.io.IOException;
+
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+
 import org.primefaces.event.SelectEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -21,10 +26,20 @@ public class Agenda {
         calendario.addEvent();
         eventoAdministrador = new Evento();
     }
+    public void realizarPago(){
+        ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+        try {
+            externalContext.redirect("../pago/index.xhtml");
+        } catch (IOException e) {
+            e.printStackTrace(); // Manejo de errores en caso de que ocurra un problema durante la redirección
+        }
+    }
     public void agendarCitaUsuario(){
         calendario.setEvent(eventoUsuario);
         calendario.addEvent();
         eventoUsuario = new Evento();
+        
+        
     }
     public void onEventSelect(SelectEvent<Evento> selectEvent) {
         eventoAdministrador = selectEvent.getObject();
